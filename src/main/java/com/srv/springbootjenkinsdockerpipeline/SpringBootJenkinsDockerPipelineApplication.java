6c03fff7d06a4fc0a0fde27d8d837e34059671a4
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-07-17 19:04:39 -0400

    first commit

---
+++ src/main/java/com/srv/springbootjenkinsdockerpipeline/SpringBootJenkinsDockerPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.srv.springbootjenkinsdockerpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsDockerPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsDockerPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srv/springbootjenkinsdockerpipeline/SpringBootJenkinsDockerPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srv.springbootjenkinsdockerpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsDockerPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
